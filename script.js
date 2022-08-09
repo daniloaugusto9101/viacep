57b517fc78fe2c4b8d087a00ff733a9a727e237d
Danilo Augusto <danilloagt@gmail.com>
2022-08-09 06:44:10 -0300

    Treino script

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("js carrgado")
+const addressForm = document.querySelector("#address-form");
+const cepInput = document.querySelector("#cep")
+const addressInput = document.querySelector("#address")
+const cityInput = document.querySelector("#city")
+const neighborhoodInout = document.querySelector("#neighborhood");
+const regionInput = document.querySelector("#region");
+const formInputs = document.querySelectorAll("[data-input]");
+const closeButton = document.querySelector("#close-message");
+const fadeElement = document.querySelector("#fade");
+
+// Validacao cep input
+cepInput.addEventListener("keypress", (e) => {    
+    if (!checkNumber(e)) {
+        e.preventDefault();
+    }
+})
+
+function checkNumber(e) {
+    const key = String.fromCharCode(e.keyCode);
+    const onlyNumber = /[0-9]/
+    return key.match(onlyNumber) ? true : false;
+}
+
+
+
+
